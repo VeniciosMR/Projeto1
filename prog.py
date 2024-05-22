3e55741ba01d19fd3f2f527a209a72ff5e55f1ea
veniciosmr <venicioste@gmail.com>
2024-05-22 08:33:33 -0300

    Sem descrição

--- prog.py
+++ prog.py
@@ -4,4 +4,5 @@ pi = np.pi
 
 # Mais casas decimais
 print(f"Valor aproximado de pi = {pi:.4f}")
-print(f"Descrição:\nO número Pi é infinito. Por esse motivo, ele é representado \ncom reticências no fim. No entanto, muitas vezes utiliza-se\napenas a aproximação para 3,1416, ou 3,14")
+# Sem descrição
+# print(f"Descrição:\nO número Pi é infinito. Por esse motivo, ele é representado \ncom reticências no fim. No entanto, muitas vezes utiliza-se\napenas a aproximação para 3,1416, ou 3,14")
